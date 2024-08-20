8379b6b3bf5b6059f65f212d0dd1a4542bc1a429
SuiliuS <suilius@tutanota.com>
2024-08-21 00:08:34 +0200

    cleanup

--- airootfs/root/.customize_airootfs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Erstelle den Benutzer 'benutzername'
-useradd -m -G wheel -s /bin/bash lotus
-
-# Sudo-Rechte für die 'wheel'-Gruppe in der sudoers-Datei aktivieren
-echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
